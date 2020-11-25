fdb9a8c952a10b3ad047ad32ba3b01184c85ef21
Renderos <noelrenderos05@gmail.com>
2020-11-25 12:13:02 -0600

    Conexion a Base de Datos y Servlet de prueba para recuperacion

--- Project1/src/ServletController/ConexionPrueba.java
+++ Project1/src/ServletController/ConexionPrueba.java
@@ -1,44 +1,27 @@
 package ServletController;
 
-import java.io.IOException;
+import Utils.ConexionBaseDatos;
 
+import java.io.IOException;
 import java.io.PrintWriter;
 
-import javax.annotation.Resource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
-import javax.sql.*;
-import java.sql.*;
-import java.sql.ResultSet;
 
-@WebServlet(name = "ServletPrueba", urlPatterns = { "/servletprueba" })
-
-public class ServletPrueba extends HttpServlet {
+@WebServlet(name = "ConexionPrueba", urlPatterns = { "/conexionprueba" })
+public class ConexionPrueba extends HttpServlet {
     private static final String CONTENT_TYPE = "text/html; charset=windows-1252";
-    
-    //Definir o establecer el DataSource
-    @Resource(name="jdbc/Articulos")
-    private DataSource miPool;
-    
-    
-    
 
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        /*response.setContentType(CONTENT_TYPE);
-        PrintWriter out = response.getWriter();
-        out.println("<html>");
-        out.println("<head><title>ServletPrueba</title></head>");
-        out.println("<body>");
-        out.println("<p>The servlet has received a GET. This is the reply.</p>");
-        out.println("</body></html>");
-        out.close();*/
-        
         //Crear el objeto printWriter
         PrintWriter salida = response.getWriter();
         response.setContentType("text/plain");
@@ -49,21 +32,21 @@ public class ServletPrueba extends HttpServlet {
         ResultSet miResultset = null;
         
         try{
-            
-            miConexion = miPool.getConnection();
-            String miSql = "SELECT * FROM ARTICULOS";
+
+            miConexion = ConexionBaseDatos.getConexion();
+            String miSql = "SELECT * FROM ARTICULO";
             miStatement = miConexion.createStatement();
             miResultset = miStatement.executeQuery(miSql);
             
             while(miResultset.next()){
-                String nombreArticulo = miResultset.getString(0);
+                String nombreArticulo = miResultset.getString(1);
                 salida.println(nombreArticulo);
             }
             
         }catch(Exception e){
+            
             e.printStackTrace();
         }
-        
         
     }
 }
--- Project1/src/Utils/ConexionBaseDatos.java
+++ Project1/src/Utils/ConexionBaseDatos.java
@@ -11,14 +11,45 @@ import java.util.Properties;
 import oracle.jdbc.pool.OracleDataSource;
 import oracle.jdbc.OracleConnection;
 import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
 
 public class ConexionBaseDatos {  
     
   final static String DB_URL="jdbc:oracle:thin:@dbsiscompras_high?TNS_ADMIN=/Users/noelr/Desktop/WalletPrueba";
   final static String DB_USER = "ADMIN";
   final static String DB_PASSWORD = "Proyecto_TOO115";
+  
+    public static Connection getConexion(){
+            Connection con=null;
+            try{
+                
+                String driver = "oracle.jdbc.driver.OracleDriver";
+                
+                Class.forName(driver);
+                con = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+                 
+                System.out.println("CONEXION EXITOSA");
+                
+            }catch (Exception e)
+            {
+                //e.printStackTrace();
+                System.out.println("NO SE PUDO CONECTAR CON LA BASE DE DATOS, ERROR: " + e);
+            }      
+            
+            return con;    
+        }
+        
+        
+        public static void main(String args[]) {            
+            
+            ConexionBaseDatos.getConexion();
+            
+        }
+}
 
-  public static void main(String args[]) throws SQLException {
+
+
+  /*public static void main(String args[]) throws SQLException {
     Properties info = new Properties();     
     info.put(OracleConnection.CONNECTION_PROPERTY_USER_NAME, DB_USER);
     info.put(OracleConnection.CONNECTION_PROPERTY_PASSWORD, DB_PASSWORD);          
@@ -38,7 +69,7 @@ public class ConexionBaseDatos {
       System.out.println("CONEXION EXITOSA!!!!!!");
       System.out.println();
         
-      //printEmployees(connection);
+      printArticulos(connection);
         
     }catch(Exception e){
         
@@ -49,12 +80,12 @@ public class ConexionBaseDatos {
   
     //Metodo para recuperar de la BD segun Git
   
-    /*public static void printEmployees(Connection connection) throws SQLException {
+    public static void printArticulos(Connection connection) throws SQLException {
         // Statement and ResultSet are AutoCloseable and closed automatically. 
+        String sentenciaSql = "SELECT * FROM ARTICULO";
         try (Statement statement = connection.createStatement()) {      
-          try (ResultSet resultSet = statement
-              .executeQuery("select first_name, last_name from employees")) {
-            System.out.println("FIRST_NAME" + "  " + "LAST_NAME");
+          try (ResultSet resultSet = statement.executeQuery(sentenciaSql)) {
+            System.out.println("CODIGO" + "  " + "NOMBRE");
             System.out.println("---------------------");
             while (resultSet.next())
               System.out.println(resultSet.getString(1) + " "
@@ -62,4 +93,3 @@ public class ConexionBaseDatos {
           }
         }   
       }*/
-}
